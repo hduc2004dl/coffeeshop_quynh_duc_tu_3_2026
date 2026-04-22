7c31dba9dbe728d38b7d27a7a1d7322432ca56c2
QuynhNguyenVan <81961062+QuynhNguyenVan@users.noreply.github.com>
2026-04-22 08:20:08 +0700

    Create Don_hang.dart

---
+++ Don_hang.dart
@@ -0,0 +1,19 @@
+class DonHang {
+  String id;
+  String tenThucUong;
+  int gia;
+  String trangThai; 
+  DonHang({
+    required this.id,
+    required this.tenThucUong,
+    required this.gia,
+    required this.trangThai,
+  });  
+  void hienThiThongTin() {
+    print('ID: $id | Món: $tenThucUong | Giá: ${gia}k | Trạng thái: $trangThai');
+  }
+  void capNhatTrangThai(String trangThaiMoi) {
+    trangThai = trangThaiMoi;
+    print('Đơn hàng $id đã cập nhật trạng thái thành: $trangThaiMoi');
+  }
+}
